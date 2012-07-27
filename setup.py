e34fd0eb790392fc1f675cc4e4088c3c9bb6fd4a
Alexis Metaireau <alexis@notmyidea.org>
2012-07-27 17:09:40 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+install_requires = []
+
+
+with open('README.rst') as f:
+    README = f.read()
+
+
+setup(name='morveux',
+      version='0.1',
+      packages=find_packages(),
+      description=("TCP hazard"),
+      long_description=README,
+      author="Alexis Metaireau",
+      author_email="alexis@notmyidea.org",
+      include_package_data=True,
+      zip_safe=False,
+      classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 1 - Planning"],
+      install_requires=install_requires,
+      test_requires=['nose'],
+      test_suite='nose.collector',
+      entry_points="""
+      [console_scripts]
+      gamin = gamin.server:main
+      """)
