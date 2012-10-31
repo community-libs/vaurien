bb9c497dc432bed10d7bd907644ef63ee5cd5b78
Tarek Ziade <tarek@ziade.org>
2012-10-31 18:21:53 +0100

    reworked the handlers a bit

--- vaurien/handlers.py
+++ vaurien/handlers.py
@@ -1,15 +1,23 @@
+import os
+
 import gevent
+
+
+def _get_data(sock, buffer=1024):
+    data = sock.recv(1024)
+    if data == '':
+        sock.close()
+        return
+    return data
 
 
 def normal(source, dest, to_backend, name, settings, proxy):
     """Dummy handler
     """
-    data = source.recv(1024)
-    if data == '':
-        source.close()
-        return
+    data = _get_data(source)
 
-    dest.sendall(data)
+    if data != '':
+        dest.sendall(data)
 
 
 def delay(source, dest, to_backend, name, settings, proxy):
@@ -28,19 +36,25 @@ def delay(source, dest, to_backend, name, settings, proxy):
 def errors(source, dest, to_backend, name, settings, proxy):
     """Reads the packets that have been sent then throws errors on the socket.
     """
+    data = _get_data(source)
+
     if to_backend:
-        proxy.get_data(source)
         # XXX find how to handle errors (which errors should we send)
         #
         # depends on the protocol
-        dest.sendall("YEAH")
+        source.sendall(os.urandom(1000))
 
+    source.close()
+    dest.close()
 
 def hang(source, dest, to_backend, name, settings, proxy):
     """Reads the packets that have been sent then hangs.
     """
     # consume the socket and hang
-    proxy.get_data(source)
+    data = _get_data(source)
+    while data != '':
+        data = _get_data(source)
+
     while True:
         gevent.sleep(1.)
 
@@ -48,7 +62,8 @@ def hang(source, dest, to_backend, name, settings, proxy):
 def blackout(source, dest, to_backend, name, settings, proxy):
     """Don't do anything -- the sockets get closed
     """
-    return
+    source.close()
+    dest.close()
 
 
 handlers = {'normal': normal,
