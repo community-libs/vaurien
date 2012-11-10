d600b41eecdb07f7f5387aee46e1893fd92c7527
Tarek Ziade <tarek@ziade.org>
2012-11-10 13:19:14 +0100

    now simulating http specific errors with the http option

--- vaurien/handlers/error.py
+++ vaurien/handlers/error.py
@@ -1,5 +1,47 @@
 import os
+import random
+
 from vaurien.handlers.dummy import Dummy
+
+_ERRORS = {
+    500: ("Internal Server Error",
+          ('<p>The server encountered an internal error and was unable to '
+           'complete your request.  Either the server is overloaded or there '
+           'is an error in the application.</p>')),
+
+    501: ("Not Implemented",
+          ('<p>The server does not support the action requested by the '
+           'browser.</p>')),
+
+    502: ("Bad Gateway",
+          ('<p>The proxy server received an invalid response from an upstream'
+           ' server.</p>')),
+
+    503: ("Service Unavailable",
+          ('<p>The server is temporarily unable to service your request due '
+           'to maintenance downtime or capacity problems.  Please try again '
+           'later.</p>'))
+}
+
+
+_ERROR_CODES = _ERRORS.keys()
+
+_TMP = """\
+HTTP/1.1 %(code)s %(name)s
+Content-Type: text/html; charset=UTF-8
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
+<title>%(code)s %(name)s</title>
+<h1>%(name)s</h1>
+%(description)s
+"""
+
+
+def random_http_error():
+    data = {}
+    data['code'] = code = random.choice(_ERROR_CODES)
+    data['name'], data['description'] = _ERRORS[code]
+    return _TMP % data
 
 
 class Error(Dummy):
@@ -15,13 +57,19 @@ class Error(Dummy):
     """
     name = 'error'
     options = {'inject': ("Inject errors inside valid data", bool, False),
-               'warmup': ("Number of calls before erroring out", int, 0)}
+               'warmup': ("Number of calls before erroring out", int, 0),
+               'http': ("return random 50xs", bool, False)}
 
     def __init__(self, settings=None, proxy=None):
         super(Error, self).__init__(settings, proxy)
         self.current = 0
 
     def __call__(self, client_sock, backend_sock, to_backend):
+        if self.option('http') and to_backend:
+            # we'll just send back a random error
+            client_sock.sendall(random_http_error())
+            return
+
         if self.current < self.option('warmup'):
             self.current += 1
             return super(Error, self).__call__(client_sock, backend_sock,
