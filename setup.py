8fa94936a136caa4ccffd87b1e7816ca337d3123
Tarek Ziade <tarek@ziade.org>
2012-11-12 09:40:23 +0100

    cleanup

--- setup.py
+++ setup.py
@@ -14,6 +14,11 @@ with open('README.rst') as f:
     README = f.read()
 
 
+classifiers = ["Programming Language :: Python",
+               "License :: OSI Approved :: Apache Software License",
+               "Development Status :: 1 - Planning"]
+
+
 setup(name='vaurien',
       version=__version__,
       packages=find_packages(),
@@ -23,12 +28,9 @@ setup(name='vaurien',
       author_email="services-dev@lists.mozila.org",
       include_package_data=True,
       zip_safe=False,
-      classifiers=[
-        "Programming Language :: Python",
-        "License :: OSI Approved :: Apache Software License",
-        "Development Status :: 1 - Planning"],
+      classifiers=classifiers,
       install_requires=install_requires,
-      test_requires=['nose'],
+      test_requires=install_requires + ['nose'],
       test_suite='nose.collector',
       entry_points="""
       [console_scripts]
