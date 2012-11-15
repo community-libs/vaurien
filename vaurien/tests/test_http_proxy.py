13d7630296832ea19f30c7096e154fe8a60bfe44
Tarek Ziade <tarek@ziade.org>
2012-11-15 10:58:10 +0100

    added more tests

---
+++ vaurien/tests/test_http_proxy.py
@@ -0,0 +1,80 @@
+import unittest
+import requests
+import subprocess
+import sys
+import time
+
+from vaurien.client import Client
+from vaurien.util import start_proxy, stop_proxy
+
+
+_PROXY = 'http://localhost:8000'
+_SERVER = [sys.executable, '-m', 'SimpleHTTPServer',
+           '8888']
+
+# we should provide a way to set an option
+# for all handlers at once
+#
+_OPTIONS = ['--handler-delay-protocol', 'http',
+            '--handler-delay-sleep', '1',
+            '--handler-dummy-protocol', 'http',
+            ]
+
+
+class TestHttpProxy(unittest.TestCase):
+    def setUp(self):
+        self._proxy_pid = start_proxy(options=_OPTIONS)
+        self._web = subprocess.Popen(_SERVER)
+        time.sleep(.5)
+        try:
+            if self._web.poll():
+                raise ValueError("Could not start the proxy")
+
+            self.client = Client()
+
+            assert self.client.get_handler() == 'dummy'
+        except Exception:
+            self.tearDown()
+            raise
+
+    def tearDown(self):
+        stop_proxy(self._proxy_pid)
+        self._web.terminate()
+
+    def test_proxy(self):
+        # let's do a few simple request first to make sure the proxy works
+        self.assertEqual(self.client.get_handler(), 'dummy')
+        times = []
+
+        for i in range(10):
+            start = time.time()
+            try:
+                res = requests.get(_PROXY)
+            finally:
+                times.append(time.time())
+
+            self.assertEqual(res.status_code, 200)
+
+        fastest = min(times)
+
+        # now let's try the various handlers
+        with self.client.with_handler('blackout'):
+            # oh look we broke it
+            self.assertRaises(requests.ConnectionError, requests.get, _PROXY)
+            self.assertEqual(self.client.get_handler(), 'blackout')
+
+        with self.client.with_handler('delay'):
+            # should work but be slower
+            start = time.time()
+            try:
+                res = requests.get(_PROXY)
+            finally:
+                duration = time.time()
+
+            self.assertEqual(res.status_code, 200)
+            self.assertTrue(duration > fastest + 1)
+
+        # we should be back to normal
+        self.assertEqual(self.client.get_handler(), 'dummy')
+        res = requests.get(_PROXY)
+        self.assertEqual(res.status_code, 200)
--- vaurien/util.py
+++ vaurien/util.py
@@ -129,7 +129,7 @@ _PROXIES = {}
 def start_proxy(proxy_host='localhost', proxy_port=8000,
                 backend_host='localhost', backend_port=8888,
                 http=True, warmup=2,
-                http_host='localhost', http_port=8080):
+                http_host='localhost', http_port=8080, options=None):
     """Starts a proxy
     """
     proxy = '%s:%d' % (proxy_host, proxy_port)
@@ -140,6 +140,9 @@ def start_proxy(proxy_host='localhost', proxy_port=8000,
     if http:
         cmd.extend(['--http', '--http-host', http_host,
                     '--http-port', str(http_port)])
+
+    if options is not None:
+        cmd.extend(options)
 
     proc = subprocess.Popen(cmd)
     time.sleep(warmup)
