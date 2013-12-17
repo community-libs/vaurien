66ab5f0cb7ed5082ecfeb1052086d0b829385870
Matthew Hooker <mwhooker@gmail.com>
2013-12-16 19:31:28 -0800

    simpler agitation.

--- vaurien/behaviors/transient.py
+++ vaurien/behaviors/transient.py
@@ -3,30 +3,17 @@ from vaurien.util import get_data
 
 class Transient(Error):
     name = 'transient'
-    options = {'warmup': ("Number of calls before succeeding out", int, 1)}
+    options = {'agitate': ("Number of calls before succeeding", int, 1)}
     options.update(Error.options)
 
     def __init__(self):
-        super(Error, self).__init__()
+        super(Transient, self).__init__()
         self.current = 0
 
     def on_before_handle(self, protocol, source, dest, to_backend):
-        if self.current < self.option('warmup'):
+        if self.current < self.option('agitate'):
             self.current += 1
-            return super(Error, self).on_before_handle(protocol, source,
+            return super(Transient, self).on_before_handle(protocol, source,
                                                        dest, to_backend)
-
-        # read the data
-        data = get_data(source)
-        if not data:
-            return False
-
-        if not to_backend:
-            # XXX find how to handle errors (which errors should we send)
-            # depends on the protocol
-            dest.sendall(os.urandom(1000))
-
-        else:          # sending the data tp the backend
-            dest.sendall(data)
 
         return True
